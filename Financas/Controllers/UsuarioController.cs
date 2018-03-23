8f337d785754a632fe0aae1f1f858b616e7e8ba7
Glauber Soares Bianchi Gualda <glaubergualda@hotmail.com>
2018-03-23 11:10:42 -0300

    adding validation with single membership

--- Financas/Controllers/UsuarioController.cs
+++ Financas/Controllers/UsuarioController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
 
 namespace Financas.Controllers
 {
@@ -22,12 +24,19 @@ namespace Financas.Controllers
             return View();
         }
 
-        public ActionResult Adiciona(Usuario usuario)
+        public ActionResult Adiciona(UsuarioModel usuario)
         {
             if (ModelState.IsValid)
             {
-                dao.Adiciona(usuario);
-                return RedirectToAction("Index");
+                try
+                {
+                    WebSecurity.CreateUserAndAccount(usuario.Email, usuario.Senha, new { Nome = usuario.Nome });
+                    return RedirectToAction("Index");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("usuario.Invalido", e.Message);                    
+                }                
             }
             return View("Form", usuario);
         }
